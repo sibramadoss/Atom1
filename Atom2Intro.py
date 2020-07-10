62a9d4579ac54b8eb392be2f132d2b3b55e5dfb0
Sibhi Ramadoss <ssr126@scarletmail.rutgers.edu>
2020-07-10 19:48:43 -0400

    Twitter

---
+++ Atom2Intro.py
@@ -0,0 +1,72 @@
+character_name = "George"
+character_age = 50.3435
+
+print("There once was an old man named " + character_name + ",")
+print("he was " + str(character_age) + " years old.")
+print("He really liked the name" + character_name + ",")
+print("but he didn't like being " + str(character_age))
+
+
+phrase = "Giraffe Academy"
+print(phrase)
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[3])
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe" , "Elephant"))
+
+from math import *
+# Number and Expressions
+print(10 % 3)
+my_num = -5
+print(my_num)
+print(str(my_num) + " my favorite number")
+print(abs(my_num))
+print(round(3.7))
+print(ceil(3.7))
+print(sqrt(3.7))
+
+# name = input ("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello " + name + "! You are " + age + "!")
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = float(num1) + float(num2)
+# print(result)
+
+train = "Python is a language"
+print(train)
+print(train + "; a good one too.")
+print(train.lower())
+print(train.upper())
+print(train.islower())
+print(train.upper().isupper())
+print(len(train))
+print(train[12])
+print(train.index("lang"))
+print(train.replace("Java", "Python"))
+
+
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+athlete = input("Enter an athlete's name: ")
+movie = input("Enter a movie: ")
+song = input("Enter a song: ")
+music_artist = input("Enter a famous singer: ")
+tv_show = input("Enter your favorite TV Show: ")
+pet = input("Enter the name of your pet: ")
+social_media = input("Enter the name of your favorite social media app: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+print(athlete + " is my favorite soccer player")
+print(movie + " is pretty bad in my opinion")
+print("Though, I do like that song : " + song + ".")
+print("You've heard of " + social_media + ", right. I spend way too much time on that broski.")
