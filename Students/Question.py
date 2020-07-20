22ded9577383eaff93845411295ed51c9f20dce0
Sibhi Ramadoss <ssr126@scarletmail.rutgers.edu>
2020-07-20 15:18:44 -0400

    ErrorFix

--- Students/Question.py
+++ Students/Question.py
@@ -1,6 +1,12 @@
-class Question:
-    def __init__(self, prompt, answer):
-        self.prompt = prompt
-        self.answer = answer
 
-        
+from student import Question
+history = Question("How old is Sibhi?", "A")
+print(history.prompt)
+print(history.answer)
+
+
+from student import Student
+student1 = Student("Jim", "Business", 3.1, False)
+print(student1.name)
+print(student1.gpa)
+print(student1.is_on_probation)
--- Students/student.py
+++ Students/student.py
@@ -12,9 +12,7 @@ class Student:
         else:
             return False
 
-
-student1 = Student("Jim", "Business", 3.1, False)
-
-print(student1.name)
-print(student1.gpa)
-print(student1.on_honor_roll())
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
