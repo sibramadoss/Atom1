556fbdd6569d17c09fc50cb74251c243b41a993e
Sibhi Ramadoss <ssr126@scarletmail.rutgers.edu>
2020-07-23 19:37:55 -0400

    Twitter

--- Students/Question.py
+++ Students/Question.py
@@ -1,13 +1,17 @@
 
 from student import Question
-history = Question("How old is Sibhi?", "A")
+history = Question("How old is Sibhi?", "19 years old")
+social_media = Question("Which tech entrepreneur invented the social media site, Twitter", "Jack Dorsey")
 print(history.prompt)
 print(history.answer)
+print(social_media.prompt)
+print(social_media.answer)
 
 from student import Student
 student1 = Student("Thilaga", "Zoology", 3.7, False)
 student2 = Student("Sibhi", "Computer Engineering", 3.99, False)
 student3 = Student("Jarvis", "Art History", 3.49, False)
+student4 = Student("Trevor", "Communications", 3.85, True)
 print(student1.name)
 print(student1.gpa)
 print(student1.is_on_probation)
@@ -20,3 +24,7 @@ print(student3.name)
 print(student3.gpa)
 print(student3.is_on_probation)
 print(student3.on_honor_roll())
+print(student4.name)
+print(student4.gpa)
+print(student4.is_on_probation)
+print(student4.on_honor_roll())
