8f3453eacf203973a318c2063957005fcef00877
Sibhi Ramadoss <ssr126@scarletmail.rutgers.edu>
2020-07-24 20:01:03 -0400

    IndianChef

--- Students/myChef.py
+++ Students/myChef.py
@@ -13,7 +13,6 @@ myChef = Chef()
 myChef.make_special_dish()
 myChef.make_salad()
 
-
 class ChineseChef(Chef):
 
     def make_chicken(self):
@@ -34,3 +33,25 @@ myChineseChef.make_special_dish()
 myChineseChef.make_chicken()
 myChineseChef.make_fried_rice()
 myChineseChef.make_spring_rolls()
+
+class IndianChef(Chef):
+
+    def make_naan(self):
+        print("The Indian chef makes naan.")
+
+    def make_special_dish(self):
+        print("The Indian Chef makes tandoori chicken.")
+
+    def make_paneer(self):
+        print("The Indian Chef makes paneer.")
+
+myIchef = IndianChef()
+myIchef.make_naan()
+myIchef.make_chicken()
+myIchef.make_special_dish()
+myIchef.make_paneer()
+myIchef.make_salad()
+myIchef.make_naan()
+myIchef.make_chicken()
+myIchef.make_special_dish()
+myIchef.make_paneer()
