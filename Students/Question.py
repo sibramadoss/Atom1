210697168422a77e60b706a9eb931b5384735ae7
Sibhi Ramadoss <ssr126@scarletmail.rutgers.edu>
2020-07-29 16:29:04 -0400

    Sch

--- Students/Question.py
+++ Students/Question.py
@@ -9,7 +9,7 @@ social_media = Question("Which tech entrepreneur invented the social media site,
 
 from student import Student
 student1 = Student("Thilaga", "SAS : Zoology", 3.7, False)
-student2 = Student("Sibhi", "SOE : Computer Engineering", 3.99, False)
+student2 = Student("Sibhi", "SOE : Computer Engineering", 3.99, False) 
 student3 = Student("Jarvis", "SAS : Art History", 3.49, False)
 student4 = Student("Trevor", "BUS : Communications", 3.85, True)
 student5 = Student("James", "SOE : Biomedical Engineering", 3.65, True)
@@ -18,7 +18,7 @@ student7 = Student("Katalina", "BUS : Finance", 3.88, True)
 student8 = Student("Jerome", "BUS : Marketing", 3.80, False)
 student9 = Student("Frank", "SOE : Mechanical Engineering", 3.90, False)
 student10 = Student("Blake", "SAS : Computer Science", 3.70, False)
-
+student11 = Student("Dominic", "SAS : Biology", 3.97, False)
 
 print(student1.name)
 print(student1.gpa)
@@ -26,6 +26,7 @@ print(student1.is_on_probation)
 print(student1.on_honor_roll())
 print(student1.engineering())
 print(student1.years_to_manage())
+print(student1.scholar())
 
 print(student2.name)
 print(student2.gpa)
@@ -33,6 +34,8 @@ print(student2.is_on_probation)
 print(student2.on_honor_roll())
 print(student2.engineering())
 print(student2.years_to_manage())
+print(student2.scholar())
+
 
 print(student3.name)
 print(student3.gpa)
@@ -40,6 +43,7 @@ print(student3.is_on_probation)
 print(student3.on_honor_roll())
 print(student3.engineering())
 print(student3.years_to_manage())
+print(student3.scholar())
 
 print(student4.name)
 print(student4.gpa)
@@ -47,6 +51,7 @@ print(student4.is_on_probation)
 print(student4.on_honor_roll())
 print(student4.engineering())
 print(student4.years_to_manage())
+print(student4.scholar())
 
 print(student5.name)
 print(student5.gpa)
@@ -54,6 +59,7 @@ print(student5.is_on_probation)
 print(student5.on_honor_roll())
 print(student5.engineering())
 print(student5.years_to_manage())
+print(student5.scholar())
 
 print(student6.name)
 print(student6.gpa)
@@ -61,6 +67,7 @@ print(student6.is_on_probation)
 print(student6.on_honor_roll())
 print(student6.engineering())
 print(student6.years_to_manage())
+print(student6.scholar())
 
 
 print(student7.name)
@@ -69,6 +76,8 @@ print(student7.is_on_probation)
 print(student7.on_honor_roll())
 print(student7.engineering())
 print(student7.years_to_manage())
+print(student7.scholar())
+
 
 print(student8.name)
 print(student8.gpa)
@@ -76,6 +85,8 @@ print(student8.is_on_probation)
 print(student8.on_honor_roll())
 print(student8.engineering())
 print(student8.years_to_manage())
+print(student8.scholar())
+
 
 print(student9.name)
 print(student9.gpa)
@@ -83,6 +94,8 @@ print(student9.is_on_probation)
 print(student9.on_honor_roll())
 print(student9.engineering())
 print(student9.years_to_manage())
+print(student9.scholar())
+
 
 print(student10.name)
 print(student10.gpa)
@@ -90,3 +103,13 @@ print(student10.is_on_probation)
 print(student10.on_honor_roll())
 print(student10.engineering())
 print(student10.years_to_manage())
+print(student10.scholar())
+
+
+print(student11.name)
+print(student11.gpa)
+print(student11.is_on_probation)
+print(student11.on_honor_roll())
+print(student11.engineering())
+print(student11.years_to_manage())
+print(student11.scholar())
--- Students/student.py
+++ Students/student.py
@@ -8,7 +8,7 @@ class Student:
 
     def on_honor_roll(self):
         if self.gpa  >= 3.5:
-            return "Congratulations"
+            return "Congratulations, you are on the honor roll"
         else:
             return "Sorry, we are informing you today that you are not on the honor roll."
 
@@ -28,6 +28,11 @@ class Student:
         else:
             return "7 years needed"
 
+    def scholar(self):
+        if self.gpa >= 3.75 and self.is_on_probation == False:
+            return "You are eligible for a scholarship"
+        else:
+            return "You are not eligible for a scholarship"
 
 class Question:
 
