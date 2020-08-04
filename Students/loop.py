673ec9a0c486db4e29bb394375100fe955b242ab
Sibhi Ramadoss <ssr126@scarletmail.rutgers.edu>
2020-08-04 19:12:39 -0400

    Lo1

--- Students/loop.py
+++ Students/loop.py
@@ -12,3 +12,22 @@ def jailbreak(phrase):
     return new_phrase
 
 print(jailbreak(input("Enter a password: ")))
+
+
+social = ["twitter", "facebook", "instagram", "reddit"]
+
+for index in social:
+    if "a" in index:
+        print index + " stops"
+    else:
+        print index + " continues"
+
+nba = ["Lebron James(AS/USA) ", "Kevin Durant(AS/USA)", "Carmelo Anthony(USA)", "Jordan Clarkson(USA)", "Giannis Antetokounmpo(AS/GRE)", "Chris Paul(AS/USA)", "Draymond Green(USA)", "Kyle Korver(USA)", "Marco Belinelli(ARG)"]
+
+for index in nba:
+    if "AS/USA" in index:
+        print index + " has qualified for the US Olympic basketball team."
+    elif "AS" in index:
+        print index + " has qualified for an Olympic basketball team."
+    else:
+        print index + " has not qualified for an Olympic basketball team."
